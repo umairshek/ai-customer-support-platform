c30837c004f16eec6c54d9a940779163044865c9
Hello.c <152676832+Anishshelar@users.noreply.github.com>
2025-05-25 16:04:07 +0530

    Create sever.js

---
+++ backend/sever.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Route connection
+const companyRoutes = require("./routes/companyRoutes");
+app.use("/api", companyRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
